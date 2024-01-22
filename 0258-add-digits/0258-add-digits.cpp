355876364f7622683fbac4ee6cf1d8897fd36f47
Ashwani Agrawal <110562552+coder-ashwani@users.noreply.github.com>
2024-01-22 14:46:26 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (6.66%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -3,9 +3,11 @@ public:
     int addDigits(int num) {
     //   The O(1) approach method is called Digit Root
     if(num==0) return 0;
-    else if(num%9==0)
-        return 9;
-    else
-        return num%9;
+    // else if(num%9==0)
+    //     return 9;
+    // else
+    //     return num%9;
+        
+    return num%9==0?9:num%9;
     }
 };
