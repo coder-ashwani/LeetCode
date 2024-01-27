8aac1aef9fab16382083a33881b2b0b76221f8f6
Ashwani Agrawal <110562552+coder-ashwani@users.noreply.github.com>
2024-01-28 03:17:25 +0530

    Time: 93 ms (77.30%), Space: 75.4 MB (6.00%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    //    int n=nums.size();
-    //    for(int i=0;i<n-1;i++){
-    //        for(int j=i+1;j<n;j++){
-    //            if(nums[i]==nums[j]){
-    //                return true;
-    //            }
-    //        }
-    //    } 
-    //    return false;
+   
 
     unordered_map<int,int> mp;
     for(auto i : nums) mp[i]++;
